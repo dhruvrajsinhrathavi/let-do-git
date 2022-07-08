92f314a738683f0565555751b5fc9d8746b4fe1b
dhruvrajsinh rathavi <dhruvrajsinhrathavi@gmail.com>
2022-07-08 18:35:27 +0530

    second commit

---
+++ diff.java
@@ -0,0 +1,9 @@
+public class diff {
+    public static void main(String[] args)
+    {
+        int a=10,b=5;
+        int differ=a-b;
+        System.out.println(differ);
+    }
+    
+}
---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply {
+    
+    public static void main(String[] args)
+    {
+        int a=10,b=5;
+        int multiply=a*b;
+        System.out.println(multiply);
+    }
+}
